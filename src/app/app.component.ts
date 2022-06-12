bd0f1c08efa29ee7437224941d828de0e55a2d44
anju-chauhan <anju.chauhan@roadcast.net>
2022-06-13 01:59:58 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+      imports: [
+         FormsModule
+      ]
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,67 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'laptop';
+  title = 'array';
+  // inputData: any = {name: ""}
+
+  name = '';
+  branch = '';
+  course = '';
+
+  student =[ 
+     {name:'anju',rollno:21,branch:'it',course:'btech'}, 
+     {name:'noor',rollno:26,branch:'it',course:'btech'}, 
+     {name:'kk',rollno:20,branch:'it',course:'btech'}, 
+     ];
+
+     data =[1,2,3,4,5];
+
+     abc ={name:'anju',addess:'bijnor'};
+
+
+     head = "This is header"
+     headData = {name: "yes", data: "this is headData"}
+     num = 345
+     price = 54
+     product = {name: "redme", price: 40000}
+     collection = [{name: "redmin 8", price: 4000},{name: "redmi 9", price: 1000}];
+
+     clickFun(){
+      this.student.push({name: this.name, rollno: 32, branch: this.branch, course: this.course})
+      // dfd
+      // this.name = ''
+      // this.branch = ''
+      // this.course = ''
+      this.name= 'enter data'
+      this.branch= 'enter data'
+      this.course= 'enter data'
+         }
+     deleteData(){
+    this.student.pop()
+}
+
+clickRun(){
+  this.student.shift();
+}
+clicktun(){
+  this.student.unshift({name:this.name,rollno:33,branch:this.branch,course:this.course});
+  this.name = ''
+      this.branch = ''
+      this.branch = ''
+      this.course = ''
+
+}
+
+clickDun(){
+  delete this.student[2];
+}
+
+     
+
+
+
+
+
+
+
 }
